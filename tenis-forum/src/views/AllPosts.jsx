1a57571b6b67bda2c03ae29e24a4142fb6877895
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-02-12 18:55:03 +0200

    a bit of css

--- tenis-forum/src/views/AllPosts.jsx
+++ tenis-forum/src/views/AllPosts.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { getAllPosts } from "../services/post-service";
 import { useSearchParams } from "react-router-dom";
 import PostInfo from "../components/SinglePost/PostInfo";
+import { Breadcrumb, Layout } from 'antd';
+
+const { Content } = Layout;
 
 
 const AllPosts = () => {
@@ -20,25 +23,39 @@ const AllPosts = () => {
     }, [search])
 
     const togglePostLike = (username, id) => {
-       setPosts(posts.map(post => {
-        if(post.id === id){
-            post.liked = post.liked.includes(username) ? post.liked.filter(u => u !== username) : [...post.liked, username];
+        setPosts(posts.map(post => {
+            if (post.id === id) {
+                post.liked = post.liked.includes(username) ? post.liked.filter(u => u !== username) : [...post.liked, username];
+            }
+            return post;
         }
-        return post;
-       }
 
-       )) 
+        ))
     }
 
     return (
         <div>
+
             <h1>All Posts</h1>
             <label htmlFor="search">Search</label>
             <input value={search} onChange={e => setSearch(e.target.value)} type="text" name="search" id="search" /> <br />
-            {posts.map((post) => (
-                <PostInfo key={post.id} post={post} togglePostLike={togglePostLike}/>
-            ))}
-            
+            <Content
+                style={{
+                    padding: 24,
+                    margin: 0,
+                    minHeight: 280,
+                    background: '#000',
+                    borderRadius: `10px`,
+                    border: `1px solid #fff`,
+                }}
+            >
+
+                {posts.map((post) => (
+                    <PostInfo key={post.id} post={post} togglePostLike={togglePostLike} />
+                ))}
+            </Content>
+
+
         </div>
     );
 };
