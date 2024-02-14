99d79a3757d2dfb4792aef2dc4490d32736c0959
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-02-14 09:53:18 +0200

    more css

--- tenis-forum/src/components/SinglePost/PostInfo.jsx
+++ tenis-forum/src/components/SinglePost/PostInfo.jsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from 'react';
 import { getUserByUserName } from '../../services/user-service';
 
 
-
 /**
  * 
  * @param {{post: {id: string, title: string, content: string, createdOn: string, liked: array, author: string}, togglePostLike: function}} props
@@ -21,25 +20,30 @@ const PostInfo = ({ post }) => {
 
     const navigate = useNavigate();
     console.log(author);
-    
+
     return (
         <div className="flex justify-center items-center h-auto mb-4 ">
             <div className="card w-3/4 h-auto bg-base-100 border border-white shadow-lg shadow-white transform transition duration-500 ease-in-out hover:scale-105 hover:shadow-2xl">
-                <div className="card-body cursor-pointer" onClick={() => navigate(`/posts/${post.id}`)}>
-                    <div className="title ">
-                        <h2 className="text-center">{post.title}</h2>
-                    </div>
-                    <div className="content">
-                        <p>Random text to test</p>
-                    </div>
-                    <div className="w-24 rounded text-center">
-                        <img src={author?.avatar} alt="" />
-                        <div className='user-info'>
+                <div className="card-normal cursor-pointer flex" onClick={() => navigate(`/posts/${post.id}`)}>
+                    <div className='w-1/4'>
+                        <div className="avatar ml-4 mb-4 mt-4">
+                            <div className="w-24 mask mask-squircle">
+                                <img src={author?.avatar} />
+                            </div>
+                        </div>
+                        <div className='user-info text-sm ml-4 mb-4'>
                             <p >{`Posted by: ${post.author}`}</p>
                             <p >{`Posted on: ${post.createdOn}`}</p>
                         </div>
                     </div>
-
+                    <div className='w-3/4'>
+                        <div className="title ">
+                            <h1 className="text-start">{post.title}</h1>
+                        </div>
+                        <div className="content text-start mt-4">
+                            <p>Random text to test hasdjkashdkjashdkjashdkjashdka;hsjdoil;ahjsdoi;ujasoid;jas;lkdja;lskdja;lksdjasd</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -60,9 +64,3 @@ PostInfo.propTypes = {
 }
 
 export default PostInfo;
-
-
-{/* <h2 className="card-title self-start">{post.title}</h2>
-                        <p className="self-center">{`${post.content.substring(0, 35)} ...`}</p>
-                        <p >{`Posted by: ${post.author}`}</p>
-                        <p >{`Posted on: ${post.createdOn}`}</p> */}
