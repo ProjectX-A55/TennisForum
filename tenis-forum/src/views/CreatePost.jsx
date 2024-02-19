64501fd334da0daa41f7365c858a626dc986f32b
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-02-19 21:42:57 +0200

    admin panel is ready

--- tenis-forum/src/views/CreatePost.jsx
+++ tenis-forum/src/views/CreatePost.jsx
@@ -23,7 +23,6 @@ const CreatePost = () => {
         updatePost(selectedOptions || [], 'tags');
     }
 
-    //TODO: Fix the condition for the title and content
     const createPost = async () => {
         if (post.title.length < 16 || post.title.length > 64) {
             alert('Title must be between 16 and 64 characters');
@@ -51,29 +50,8 @@ const CreatePost = () => {
         navigate('/posts');
     };
 
-    // const options = [
-    //     { value: 'Players', label: 'Players' },
-    //     { value: 'Tournaments', label: 'Tournaments' },
-    //     { value: 'Techniques', label: 'Techniques' },
-    //     { value: 'Gear', label: 'Gear' },
-    //     { value: 'Matches', label: 'Matches' },
-    //     { value: 'Strategy', label: 'Strategy' },
-    //     { value: 'Training', label: 'Training' },
-    //     { value: 'Fitness', label: 'Fitness' },
-    //     { value: 'News', label: 'News' },
-    //     { value: 'Discussion', label: 'Discussion' },
-    //     { value: 'Tips', label: 'Tips' },
-    //     { value: 'Equipment', label: 'Equipment' },
-    //     { value: 'Coaching', label: 'Coaching' },
-    //     { value: 'Events', label: 'Events' },
-    //     { value: 'Community', label: 'Community' },
-    // ];
-
-    // const themeColor1 = getComputedStyle(document.documentElement).getPropertyValue('--theme-color-1');
-    // const themeColor2 = getComputedStyle(document.documentElement).getPropertyValue('--theme-color-2');
-
+   
     return (
-
         <div className="relative w-full flex flex-col  justify-center ">
             <div className="w-full p-6 m-auto bg-gray rounded-md shadow-2xl ring-2 ring-white lg:max-w-xl" style={{ borderColor: '#654321' }}>
                 <h1 className="text-3xl font-semibold text-center">Create a post</h1>
@@ -93,20 +71,6 @@ const CreatePost = () => {
                 </div><br />
                 <div>
                     <TagsInput updateTags={updateTags} alreadySelectedTags={post.tags}/>
-                    {/* <label htmlFor="input-tags">Tags</label>
-                    <ReactSelect
-                        isMulti
-                        options={options}
-                        value={post.tags.map(tag => ({ value: tag.value, label: tag.label }))}
-                        onChange={selectedOptions => updatePost(selectedOptions || [], 'tags')}
-                        styles={{
-                            control: (provided) => ({ ...provided, backgroundColor: themeColor1, color: themeColor2 }),
-                            option: (provided) => ({ ...provided, backgroundColor: themeColor1, color: themeColor2 }),
-                            multiValue: (provided) => ({ ...provided, backgroundColor: themeColor1, color: themeColor2 }),
-                            multiValueLabel: (provided) => ({ ...provided, color: themeColor2 }),
-                            multiValueRemove: (provided) => ({ ...provided, color: themeColor2 }),
-                        }}
-                    /> */}
                 </div>
                 <br />
                 <div>
