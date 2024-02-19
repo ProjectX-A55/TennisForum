f8e9985f8762018b4103921c4828b93fa068b7db
Desislava Velinova <desislavatsvelinova@gmail.com>
2024-02-19 16:26:35 +0200

    same

--- tenis-forum/src/components/Footer/Footer.jsx
+++ tenis-forum/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-    //TODO: TRQBVA DA SMENIM TEKSTA NA FOOTERA
+
     return (
         <footer className="rounded-lg footer footer-center p-4 bg-base-300 text-base-content mt-10">
             <aside>
