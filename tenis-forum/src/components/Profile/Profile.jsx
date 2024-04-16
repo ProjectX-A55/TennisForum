70fc4e985a4b0b560ee8eb2e70f1035d0357bf8f
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-04-16 18:06:48 +0300

    bit css

--- tenis-forum/src/components/Profile/Profile.jsx
+++ tenis-forum/src/components/Profile/Profile.jsx
@@ -17,7 +17,7 @@ const Profile = () => {
     }
 
     return (
-        <div className="flex flex-col lg:flex-row lg:space-x-5">
+        <div className="flex flex-col lg:flex-row lg:space-x-10">
             <aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:w-1/6 ml-5 mb-5 lg:mb-0">
                 <div className="w-full lg:w-56">
                     <ul className="menu w-full lg:w-56 rounded-box border border-amber-950 shadow-2xl">
@@ -27,7 +27,7 @@ const Profile = () => {
                     </ul>
                 </div>
             </aside>
-            <div className="w-full ml-5 lg:ml-0">
+            <div className="w-full lg:ml-0">
                 {componentsMap[state]}
             </div>
         </div>
